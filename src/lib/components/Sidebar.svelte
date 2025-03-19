ea80248e527895afe0c8ed58bc50c8d12a6cab6f
Nicholas Marino <nicholas.marino@zero.nyc>
2025-03-19 15:11:20 -0400

    Style updates

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -2,7 +2,7 @@
  let { heading, description } = $props();
 </script>
 
-<section class="px-8 py-16 absolute z-20 w-1/3 h-screen overflow-scroll bg-black">
+<section class="p-4 absolute left-4 bottom-4 z-20 w-[600px] h-fit bg-black border border-white">
     <h1 class="h5 uppercase mb-8">{heading}</h1>  
     <p class="mb-4">{description}</p>
     <p><a href="/" class="w-10 h-10 block rounded-full bg-white flex items-center justify-center back-btn"><span>🔙</span></a></p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
     import content from '$lib/content';
 </script>
-<section class="p-8 h-screen overflow-scroll">
-    <div class="h-[120px] bg-black fixed top-0 z-20 mb-8 flex items-center">
-        <h1 class="uppercase text-white">Arboretum&nbsp;<a href="https://www.zero.nyc" target="_blank" class="zero"></a></h1>
+<section class="p-8 h-screen">
+    <div class="h-[200px] bg-black fixed top-0 z-20 mb-8 flex items-center">
+        <h1 class="h3 uppercase text-white">Zero&nbsp;<a href="https://www.zero.nyc" target="_blank" class="blink zero"></a>&nbsp;Arboretum</h1>
     </div>
-    <ul class="text-white flex flex-col gap-12 my-[120px]">
+    <ul class="text-white flex flex-col gap-12 my-[200px]">
         {#each content as demo}
             <li>
                 <a href={demo.slug} class="polaroid">
@@ -19,12 +19,28 @@
 
 <style>
     .zero {
-        width: 30px;
-        height: 30px;
+        width: 25px;
+        height: 25px;
         background-color: white;
         display: inline-block;
         border-radius: 100%;
     }
+    .blink {
+    animation: blink-animation 1.5s ease-in-out infinite;
+    }
+
+@keyframes blink-animation {
+    0%, 100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.5;
+    transform: scale(1.1);
+  }
+}
+
+
     .polaroid {
         text-decoration: none;
         display: flex;        
